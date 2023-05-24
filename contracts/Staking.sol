c72d640b3e72716346d723c077fe8d4af31dbee3
Adam O'Callaghan <adam.ocallaghan@yahoo.ie>
2023-05-24 15:19:18 +0100

    First commit of staking

---
+++ contracts/Staking.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.19;
+
+import "../node_modules/@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "../node_modules/@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+contract Staking {
+
+    /*  
+        ==============================================
+        === STATE VARIABLES, EVENTS & CONTSTRUCTOR ===
+        ==============================================
+    */
+
+    bool internal locked; // reentrancy lock
+
+    // Libraries
+    using SafeERC20 for IERC20;
+    using SafeMath for uint256;
+
+    address public owner;
+
+    // Timestamps
+    uint256 public initialTimestamp;
+    bool public timestampSet;
+    uint256 public timePeriod;
+
+    // Balances
+    mapping(address => uint256) public alreadyWithdrawn;
+    mapping(address => uint256) public balances;
+    uint256 public contractBalance;
+
+    // Stakable token
+    IERC20 public erc20Contract;
+
+    // Events
+    event tokensStaked(address from, uint256 amount);
+    event tokensUnstaked(address to, uint256 amount);
+
+    // Constructor
+    constructor(IERC20 _erc20_contract_address) {
+        owner = msg.sender;
+        timestampSet = false;
+        require(address(_erc20_contract_address) != address(0), "Can't be zero address");
+        erc20Contract = _erc20_contract_address;
+        locked = false;
+    }
+
+    /*  
+        =============================================
+        ================= FUNCTIONS =================
+        =============================================
+    */
+
+    
+
+
+
+    /*  
+        =============================================
+        ================= MODIFIERS =================
+        =============================================
+    */    
+
+}
